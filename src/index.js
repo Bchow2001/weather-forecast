8cadde60a2fc05e33cc8650ba802fdd1bd7acf9c
Bryan Chow <bchow.post@gmail.com>
2023-08-07 18:51:24 +0100

    Basic functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import "./style.css";
 
+const submitBtn = document.querySelector("#submit");
+const input = document.querySelector("#location");
+const form = document.querySelector("#form");
+
 function fetchForecast(location) {
 	fetch(
-		`http://api.weatherapi.com/v1/forecast.json?key=7ed75af930ca4051994152454232007&q=${location}`,
+		`http://api.weatherapi.com/v1/forecast.json?key=7ed75af930ca4051994152454232007&q=${location}&days=5`,
 		{
 			mode: "cors",
 		},
-	).then((response) => {
-		const data = response.json();
-		console.log(data);
-	});
+	)
+		.then((response) => {
+			const data = response.json();
+			return data;
+		})
+		.then((data) => {
+			const forecastData = data.forecast.forecastday;
+			console.log(forecastData);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
 }
 
-fetchForecast("london");
+submitBtn.addEventListener("click", () => {
+	if (input.value.length > 3) {
+		fetchForecast(input.value);
+	} else {
+		form.reportValidity();
+	}
+});
