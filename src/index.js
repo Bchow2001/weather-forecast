d7fedbf3624173aaf7ac76f94193f300194bb29e
Bryan Chow <bchow.post@gmail.com>
2023-08-16 11:41:56 +0100

    Change API call to HTTPS

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ form.addEventListener("submit", (e) => {
 
 function fetchForecast(location) {
 	fetch(
-		`http://api.weatherapi.com/v1/forecast.json?key=7ed75af930ca4051994152454232007&q=${location}&days=8`,
+		`https://api.weatherapi.com/v1/forecast.json?key=7ed75af930ca4051994152454232007&q=${location}&days=8`,
 		{
 			mode: "cors",
 		},
